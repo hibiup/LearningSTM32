41772e929dba588be13c9c03e37d0ace4cb62985
Jeff <jeffwji@gmail.com>
2019-02-27 22:11:16 -0500

    Update macro

--- HelloLib/User/bsp_led.h
+++ HelloLib/User/bsp_led.h
@@ -39,52 +39,28 @@
 
 /* 基本混色，后面高级用法使用PWM 可混出全彩颜色,且效果更好 */
 //红
-#define LED_RED \
-        LED1_ON;\
-        LED2_OFF\
-        LED3_OFF
+#define LED_RED LED1_ON; LED2_OFF; LED3_OFF
 
 //绿
-#define LED_GREEN \
-        LED1_OFF;\
-        LED2_ON\
-        LED3_OFF
+#define LED_GREEN LED1_OFF; LED2_ON; LED3_OFF
 
 //蓝
-#define LED_BLUE \
-        LED1_OFF;\
-        LED2_OFF\
-        LED3_ON
+#define LED_BLUE LED1_OFF; LED2_OFF; LED3_ON
 
 //黄(红+绿)
-#define LED_YELLOW \
-        LED1_ON;\
-        LED2_ON\
-        LED3_OFF
+#define LED_YELLOW LED1_ON; LED2_ON; LED3_OFF
 
 //紫(红+蓝)
-#define LED_PURPLE \
-        LED1_ON;\
-        LED2_OFF\
-        LED3_ON
+#define LED_PURPLE LED1_ON; LED2_OFF; LED3_ON
 
 //青(绿+蓝)
-#define LED_CYAN \
-        LED1_OFF;\
-        LED2_ON\
-        LED3_ON
+#define LED_CYAN LED1_OFF; LED2_ON; LED3_ON
 
 //白(红+绿+蓝)
-#define LED_WHITE \
-        LED1_ON;\
-        LED2_ON\
-        LED3_ON
+#define LED_WHITE  LED1_ON; LED2_ON; LED3_ON
 
 //黑(全部关闭)
-#define LED_RGBOFF \
-        LED1_OFF;\
-        LED2_OFF\
-        LED3_OFF
+#define LED_RGBOFF LED1_OFF; LED2_OFF; LED3_OFF
 
 
 
